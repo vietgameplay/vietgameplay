4880d85b40975a8eb91a5812fb8e79be90951009
ndtaibk07t4 <ndtaibk07t4@833c858a-48d5-f3bb-e5d2-89196ec81be4>
2014-05-24 14:16:00 +0000

    - create transition

--- projects/cocos2dx/samples/Cpp/TemplateGame/Classes/MainMenuScene.cpp
+++ projects/cocos2dx/samples/Cpp/TemplateGame/Classes/MainMenuScene.cpp
@@ -14,14 +14,20 @@ CCScene* MainMenuScene::scene()
     // add layer as a child to scene
     scene->addChild(layer);
 
+	CCScene* tran_Scene = CCTransitionFlipX::create(0.5f, scene );
+
     // return the scene
-    return scene;
+    return tran_Scene;
 }
 
 // on "init" you need to initialize your instance
 void MainMenuScene::onEnter()
 {
 	CCLayer::onEnter();  
+	CCSprite* sprite = CCSprite::create("HelloWorld.png");
+	sprite->setPosition( CCPoint ( 400, 240 ) );
+	addChild( sprite );
+
 }
 
 void MainMenuScene::update ( float dt )

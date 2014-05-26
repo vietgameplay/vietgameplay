67334679804588136012f30061d94803d416f6c8
ndtaibk07t4 <ndtaibk07t4@833c858a-48d5-f3bb-e5d2-89196ec81be4>
2014-05-26 15:19:20 +0000

    - Example Menu & ItemMenu

--- projects/cocos2dx/samples/Cpp/TemplateGame/Classes/VGPLib/VGPSprite.h
+++ projects/cocos2dx/samples/Cpp/TemplateGame/Classes/VGPLib/VGPSprite.h
@@ -37,59 +37,5 @@ public:
 
 	void addFrame (  CCLayer* layer, char *pszFileName, CCRect rect, CCPoint position, CCPoint anchor = CCPoint ( 0.5f, 0.5f ) );	
 
-	/*
-
-	CCSprite* createAnimation ( int spriteID, int animID, 
-								float delay, int loop, CCPoint position, int zOrder, int tag );
-
-	CCSprite* createAnimation ( int spriteID, int animID, 
-								float delay, int loop, CCPoint position, int zOrder );
-
-	CCSprite* createAnimation ( int spriteID, int animID, 
-								float delay, int loop, CCPoint position);	
-
-	void addAnimation ( CCLayer* layer, int spriteID, int animID, 
-								float delay, int loop, CCPoint position, int zOrder, int tag );
-
-	void addAnimation ( CCLayer* layer, int spriteID, int animID, 
-								float delay, int loop, CCPoint position, int zOrder );
-
-	void addAnimation ( CCLayer* layer, int spriteID, int animID, 
-								float delay, int loop, CCPoint position);	
-
-
-	CCSprite* createFrameMove ( int spriteID, int frameID, CCPoint srcPosition, 
-								CCPoint desPosition, int jumpStep, int zOrder, int tag );
-
-	CCSprite* createFrameMove ( int spriteID, int frameID, CCPoint srcPosition, 
-								CCPoint desPosition, int jumpStep, int zOrder );
-
-	CCSprite* createFrameMove ( int spriteID, int frameID, CCPoint srcPosition, 
-								CCPoint desPosition, int jumpStep );
-								
-
-	void addFrameMove ( CCLayer* layer, int spriteID, int frameID, CCPoint srcPosition, 
-								CCPoint desPosition, int jumpStep, int zOrder, int tag );
-
-	void addFrameMove ( CCLayer* layer, int spriteID, int frameID, CCPoint srcPosition, 
-								CCPoint desPosition, int jumpStep, int zOrder );
-
-	void addFrameMove ( CCLayer* layer, int spriteID, int frameID, CCPoint srcPosition, 
-								CCPoint desPosition, int jumpStep );
-
-
-	CCMenuItemSprite* createButton ( int spriteID, int frameID, int frameSelectID, CCPoint position, int zOrder, int tag );
-
-
-	CCMenuItemSprite* createButton ( int spriteID, int frameID, int frameSelectID, CCPoint position, int zOrder );
-
-	CCMenuItemSprite* createButton ( int spriteID, int frameID, int frameSelectID, CCPoint position );
-	
-	void addButton ( CCLayer* layer, int spriteID, int frameID, int frameSelectID, CCPoint position, int zOrder, int tag );
-
-	void addButton ( CCLayer* layer, int spriteID, int frameID, int frameSelectID, CCPoint position, int zOrder );
-
-	void addButton ( CCLayer* layer, int spriteID, int frameID, int frameSelectID, CCPoint position );
-	*/
 };
 #endif

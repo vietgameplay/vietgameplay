74eddb15001b9e177896d37706af7589334aa9d6
ndtaibk07t4 <ndtaibk07t4@833c858a-48d5-f3bb-e5d2-89196ec81be4>
2014-10-30 08:11:57 +0000

    Example 2: create a state use scene and layer Change LogoScene:  Inheritance from LayerColor  Init with onEnter function  Init with white color Create MainMenuScene  The same LogoScene structure  Inheritance from Layer Logo appears 3s then switch to MainMenu  Use time system  Use non time system ( frame count )

--- projects/cocos2dx3.2/projects/exampleCocos2dx/Classes/LogoScene.cpp
+++ projects/cocos2dx3.2/projects/exampleCocos2dx/Classes/LogoScene.cpp
@@ -1,5 +1,6 @@
 #include "Defines.h"
 
+extern int s_frameCount;
 
 Scene* LogoScene::createScene()
 {
@@ -17,74 +18,34 @@ Scene* LogoScene::createScene()
 }
 
 // on "init" you need to initialize your instance
-bool LogoScene::init()
+void LogoScene::onEnter()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Layer::init() )
-    {
-        return false;
-    }
-    
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	LayerColor::onEnter();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+	s_frameCount = 0;
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(LogoScene::menuCloseCallback, this));
-    
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+	LayerColor::initWithColor( Color4B(255, 255, 255 , 255) );
+	Sprite *logo = Sprite::create( IMAGE_LOGO );
+	logo->setPosition( BASE_SCREEN_HALF_W, BASE_SCREEN_HALF_H );
+	this->addChild( logo );
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
+	//update for 3s -------------------------------------------
+	//this->schedule( schedule_selector( LogoScene::update ), TIME_LOGO );
 
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = LabelTTF::create("Hello World", "Arial", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
-    return true;
+	////update for each frame ---------------------------------
+	this->schedule( schedule_selector( LogoScene::update ) );
 }
 
-
-void LogoScene::menuCloseCallback(Ref* pSender)
+void LogoScene::update( float dt )
 {
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
-    return;
-#endif
+	//--------------------------
+	//call immediately the first into -> 3s 
+	//Director::sharedDirector()->replaceScene( MainMenuScene::createScene() );
 
-    Director::getInstance()->end();
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
+	//--------------------------
+	s_frameCount++;
+	//switch state
+	if ( s_frameCount == FPS*TIME_LOGO )
+		Director::sharedDirector()->replaceScene( MainMenuScene::createScene() );
 }
+
--- projects/cocos2dx3.2/projects/exampleCocos2dx/Classes/MainMenuScene.h
+++ projects/cocos2dx3.2/projects/exampleCocos2dx/Classes/MainMenuScene.h
@@ -1,21 +1,22 @@
-#ifndef __LOGOSCENE_SCENE_H__
-#define __LOGOSCENE_SCENE_H__
+#ifndef __MAINMENU_SCENE_H__
+#define __MAINMENU_SCENE_H__
 
 #include "cocos2d.h"
 
-class LogoScene : public cocos2d::LayerColor
+class MainMenuScene : public cocos2d::Layer
 {
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
 
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual void onEnter();  
-
-    virtual void update( float dt );  
-
+	virtual void onEnter();  
+    
+    // a selector callback
+    void menuCloseCallback( cocos2d::Ref* pSender );
+    
     // implement the "static create()" method manually
-    CREATE_FUNC(LogoScene);
+    CREATE_FUNC( MainMenuScene );
 };
 
 #endif // __HELLOWORLD_SCENE_H__

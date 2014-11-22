d58481e0697047cff0da451ff3578f1fc6883f96
ndtaibk07t4 <ndtaibk07t4@833c858a-48d5-f3bb-e5d2-89196ec81be4>
2014-11-22 09:19:38 +0000

    + add new project: TAP TAP

---
+++ projects/cocos2dx3.2/projects/taptap/Classes/AppDelegate.cpp
@@ -0,0 +1,59 @@
+#include "Defines.h"
+
+AppDelegate::AppDelegate() {
+
+}
+
+AppDelegate::~AppDelegate() 
+{
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+    // initialize director
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+        glview = GLView::create("My Game");	
+        director->setOpenGLView(glview);
+    }
+	#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+	glview->setFrameSize(BASE_SCREEN_W, BASE_SCREEN_H);
+	#endif //only set fit on win32		
+
+	#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+	hideBannerGoogle();
+	hideBannerStartApp();
+	#endif
+
+	glview->setDesignResolutionSize(BASE_SCREEN_W, BASE_SCREEN_H, ResolutionPolicy::EXACT_FIT );
+
+    // turn on display FPS
+    director->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0 / FPS);
+
+    // create a scene. it's an autorelease object
+    auto scene = LogoScene::createScene();
+
+    // run
+    director->runWithScene(scene);
+
+    return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground() {
+    Director::getInstance()->stopAnimation();
+
+    // if you use SimpleAudioEngine, it must be pause
+    // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground() {
+    Director::getInstance()->startAnimation();
+
+    // if you use SimpleAudioEngine, it must resume here
+    // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+}
